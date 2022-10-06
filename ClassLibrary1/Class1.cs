50f2e17b9e78cd7c5709fd6818a575fe1ad7f40e
P129CBD <P129CBD@L-PW02X07Y>
2022-10-06 09:14:57 +0400

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public static void TurnaryExp()
+        {
+            var value=int.Parse(Console.ReadLine());
+            string msg=string.Empty;
+
+            msg = value < 10 ? "Value less than 10" : "Value greater than 10";
+            Console.WriteLine(msg);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ClassLibrary1/Labsheet1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Labsheet1
+    {
+
+        public static void Display()
+        {
+            int previous = 0;
+            foreach (var cat in Enum.GetValues(typeof(Category)))
+            {
+                Console.WriteLine("Category " + cat + " has the range of " + previous + " - " + (int)cat);
+                previous = (int)cat + 1;
+
+            }
+        }
+        public static void Check()
+        {
+            int value = 0;
+
+            do {
+                Display();
+
+                Console.Write("Enter a value: ");
+                value = int.Parse(Console.ReadLine());
+
+                if (value == -1)
+                    break;
+                
+
+                Category userCat = new Category();
+                int previous=0; 
+
+                foreach (var cat in Enum.GetValues(typeof(Category)))
+                {
+                   
+                    if (value >= previous && value <= (int)cat)
+                    {
+                        userCat = (Category)cat;
+                        break;
+                    }
+                    previous = (int)cat + 1;
+
+                }
+               
+                Console.WriteLine("Your category: "+ userCat);
+               
+
+            }while(value != -1);
+
+
+            
+        }
+    }
+}
